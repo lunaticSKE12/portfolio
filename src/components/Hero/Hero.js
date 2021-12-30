e85471298bee28c6fdb52b45879de4315a02b759
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-12-30 16:55:26 +0700

    Header and Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Porfolio
+      </SectionTitle>
+      <SectionText>asdfjklasdfjkasl</SectionText>
+      <Button onClick={() => (window.location = 'https://google.com')}>
+        Learn more
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
